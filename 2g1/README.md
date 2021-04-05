1b6eb4087b19ee9cc17ceff9486731be6ba9f571
IYATT-yx <2514374431@qq.com>
2021-04-06 02:15:41 +0800

    2021-4-6 2g1 k2 测试成功提交

--- 2g1/README.md
+++ 2g1/README.md
@@ -1,41 +1,14 @@
 # 路由器
 2g1  
-K2  
-openwrt 21.092 (自编译)  
+自编译固件  
+[K2](k2)  
+[Newifi3](newifi3)  
 
-1.下载并自行刷固件  
+将`rp-pppoe.so`上传到/root  
 
-[K2](openwrt-ramips-mt7620-phicomm_psg1218a-squashfs-sysupgrade.bin)  
-
-2.安装pppoe-server
-
-```bash
-opkg update
-```
-
-```bash
-opkg install rp-pppoe-server
-```
-
-3.运行脚本  
-
-[下载](nk)  
-
-上传  
-```bash
-scp * root@192.168.6.1:/root
-```
+[脚本](nk)
 
 运行  
 ```bash
 sh /root/nk4conf.sh
-```
-
-# 3.功能扩展
-#### (1) 客户端限速
-```bash
-opkg install luci-app-nft-qos
-```
-```bash
-opkg install luci-i18n-nft-qos-zh-cn
 ```
---
+++ 2g1/nk/nk4.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#start pppoe-server
+if [ -n "$(ps | grep pppoe-server | grep -v grep)" ]
+then
+    killall pppoe-server
+fi
+pppoe-server -k -I br-lan
+
+#clear logs
+cat /dev/null > /tmp/pppoe.log
+
+sleep 15 # wait 15s for reboot, or it will throw error for 'you have accessed in'
+logger -t nk4 "try to login at power on"
+ifdown netkeeper
+ifup netkeeper
+
+while :
+do
+    #read the last username in pppoe.log
+    if [ "$(grep 'user=' /tmp/pppoe.log | grep 'rcvd' | tail -n 1 | cut -d \" -f 5)" == "]" ]
+    then
+        username=$(grep 'user=' /tmp/pppoe.log | grep 'rcvd' | tail -n 1 | cut -d \" -f 2)
+    fi
+
+    if [ "$username" != "$username_old" ]
+    then
+        ifdown netkeeper
+        uci set network.netkeeper.username="$username"
+        uci set network.netkeeper.password="$(grep 'user=' /tmp/pppoe.log | grep 'rcvd' | tail -n 1 | cut -d \" -f 4)"
+        uci commit
+        ifup netkeeper
+        username_old="$username"
+        logger -t nk4 "new username $username"
+    fi
+    
+    sleep 10
+
+    #close pppoe if log fail
+    if [ -z "$(ifconfig | grep "netkeeper")" ]
+    then
+        ifdown netkeeper
+    fi
+
+done
